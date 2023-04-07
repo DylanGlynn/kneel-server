308fb46a84842bd5df0e3881672630829822afd5
Dylan Glynn <dylan.hawes.glynn+github@gmail.com>
2023-04-07 15:03:04 -0500

    Explorer Chapter 1.

--- views/orders.py
+++ views/orders.py
@@ -1,3 +1,7 @@
+from .metals import get_single_metal
+from .sizes import get_single_size
+from .styles import get_single_style
+
 ORDERS = [
     {
     "metalId": 1,
@@ -24,6 +28,15 @@ def get_single_order(id):
     for order in ORDERS:
         if order["id"] == id:
             requested_order = order
+            matching_metal = get_single_metal(requested_order["metalId"])
+            requested_order["metal"] = matching_metal
+            matching_size = get_single_size(requested_order["sizeId"])
+            requested_order["size"] = matching_size
+            matching_style = get_single_style(requested_order["styleId"])
+            requested_order["style"] = matching_style
+            del order["metalId"]
+            del order["sizeId"]
+            del order["styleId"]
 
     return requested_order
 
