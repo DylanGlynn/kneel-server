2d7a7c5f99e96716fe183371cb33b810824c00e2
Dylan Glynn <dylan.hawes.glynn+github@gmail.com>
2023-04-14 12:09:44 -0500

    Completed Book 2 Chapter 3.

--- views/orders.py
+++ views/orders.py
@@ -6,19 +6,20 @@ from .styles import get_single_style
 
 ORDERS = [
     {
-    "metalId": 1,
-    "sizeId": 3,
-    "styleId": 2,
-    "timestamp": 161459931693,
-    "id": 1
+        "metalId": 1,
+        "sizeId": 3,
+        "styleId": 2,
+        "timestamp": 161459931693,
+        "id": 1
     },
     {
-    "metalId": 2,
-    "sizeId": 2,
-    "styleId": 2,
-    "timestamp": 161459931693,
-    "id": 2}
+        "metalId": 2,
+        "sizeId": 2,
+        "styleId": 2,
+        "timestamp": 161459931693,
+        "id": 2}
 ]
+
 
 def get_all_orders():
     ''' Shows all ORDERS. '''
@@ -58,8 +59,8 @@ def get_all_orders():
                           row['timestamp'])
             metal = Metal(row['metal_id'], row['metal'],
                           row['metal_price'])
-            size = Size(row['size_id'],row['size'],row['size_price'])
-            style = Style(row['style_id'],row['style'],row['style_price'])
+            size = Size(row['size_id'], row['size'], row['size_price'])
+            style = Style(row['style_id'], row['style'], row['style_price'])
 
             order.metal = metal.__dict__
             order.size = size.__dict__
@@ -72,21 +73,48 @@ def get_all_orders():
 
 def get_single_order(id):
     '''Shows a single order from ORDERS.'''
-    requested_order = None
-    for order in ORDERS:
-        if order["id"] == id:
-            requested_order = order
-            matching_metal = get_single_metal(requested_order["metalId"])
-            requested_order["metal"] = matching_metal
-            matching_size = get_single_size(requested_order["sizeId"])
-            requested_order["size"] = matching_size
-            matching_style = get_single_style(requested_order["styleId"])
-            requested_order["style"] = matching_style
-            del order["metalId"]
-            del order["sizeId"]
-            del order["styleId"]
+    with sqlite3.connect("./kneeldiamonds.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    return requested_order
+        db_cursor.execute("""
+        SELECT
+            o.id,
+            o.metal_id,
+            o.size_id,
+            o.style_id,
+            o.timestamp,
+            m.metal,
+            m.price metal_price,
+            s.size,
+            s.price size_price,
+            t.style,
+            t.price style_price
+        FROM Orders o
+        JOIN Metals m
+            ON m.id = o.metal_id
+        JOIN Sizes s
+            ON s.id = o.size_id
+        JOIN Styles t
+            On t.id = o.style_id
+        WHERE o.id = ?
+        """, (id,))
+
+        data = db_cursor.fetchone()
+
+        order = Order(data['id'], data['metal_id'],
+                      data['size_id'], data['style_id'],
+                      data['timestamp'])
+        metal = Metal(data['metal_id'], data['metal'],
+                      data['metal_price'])
+        size = Size(data['size_id'], data['size'], data['size_price'])
+        style = Style(data['style_id'], data['style'], data['style_price'])
+
+        order.metal = metal.__dict__
+        order.size = size.__dict__
+        order.style = style.__dict__
+
+        return order.__dict__
 
 def create_order(order):
     '''Get the id value of the last order in the list.'''
@@ -95,6 +123,7 @@ def create_order(order):
     order["id"] = new_id
     ORDERS.append(order)
     return order
+
 
 def delete_order(id):
     '''The delete an order'''
@@ -105,6 +134,7 @@ def delete_order(id):
 
     if order_index >= 0:
         ORDERS.pop(order_index)
+
 
 def update_order(id, new_order):
     '''Handles the PUT request for order updates.'''
