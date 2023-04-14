d81ec0246f43b8ca3a6ebd143643def550de7ac1
Dylan Glynn <dylan.hawes.glynn+github@gmail.com>
2023-04-14 14:06:15 -0500

    Complete Book 2 Chapter 5.

--- views/orders.py
+++ views/orders.py
@@ -136,13 +136,13 @@ def create_order(new_order):
 
 def delete_order(id):
     '''The delete an order'''
-    order_index = -1
-    for index, order in enumerate(ORDERS):
-        if order["id"] == id:
-            order_index = index
+    with sqlite3.connect("./kneeldiamonds.sqlite3") as conn:
+        db_cursor = conn.cursor()
 
-    if order_index >= 0:
-        ORDERS.pop(order_index)
+        db_cursor.execute("""
+        DELETE FROM Orders
+        WHERE id = ?
+        """, (id, ))
 
 
 def update_order(id, new_order):
